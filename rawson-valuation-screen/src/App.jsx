cfd617a364f5e45f8987505cce5c6531965ac232
JasonJF <47773807+JasonJF@users.noreply.github.com>
2022-01-18 12:13:04 +0200

    initial commit

---
+++ rawson-valuation-screen/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <button className="btn btn-primary">
+      <i className="bi bi-plus-lg"></i>Hello Vue!</button>
+    </div>
+  )
+}
+
+export default App
