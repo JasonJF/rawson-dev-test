c9786446a2b3c13ef2d93ea0e60ad7c5ba832246
JasonJF <47773807+JasonJF@users.noreply.github.com>
2022-01-20 08:01:30 +0200

    fix: Modified SellerItem component to accept an array of sellers

--- rawson-valuation-screen/src/components/SellerItem.jsx
+++ rawson-valuation-screen/src/components/SellerItem.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 export default function SellerItem(props) {
 
     let sellers = props.value.cell.value;
+    // console.log(sellers);
     return (
-        <div className="sellerContainer d-flex">
-            <p className="text-wrap">{sellers}</p>
+        <div className="sellerContainer d-flex flex-column justify-content-center mx-auto">
+            {sellers && sellers.map((seller) => {
+               return <p className="text-wrap mx-0 px-0">{seller}</p>
+            })}
         </div>
     );
 }
