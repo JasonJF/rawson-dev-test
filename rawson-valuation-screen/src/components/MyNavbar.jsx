1b04f60d1eeeb64c25e176d93d787f5093187de0
JasonJF <47773807+JasonJF@users.noreply.github.com>
2022-01-20 05:22:27 +0200

    style: Updated nav logo styling

--- rawson-valuation-screen/src/components/MyNavbar.jsx
+++ rawson-valuation-screen/src/components/MyNavbar.jsx
@@ -5,7 +5,7 @@ export default function MyNavbar() {
         <nav className="navbar navbar-expand-sm mx-0 px-0 bg-white">
            <div className="container justify-content-between">
            <div className="d-flex">
-                <i className="bi bi-bar-chart-fill text-warning px-1 align-self-end"></i>
+                <i className="navLogo bi bi-bar-chart-fill me-2 align-self-end"></i>
                 <h4 className="align-self-center my-0 px-1">Valuations</h4>
             </div>
             <div className="d-flex justify-content-end" >
